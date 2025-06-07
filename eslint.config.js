28f559812bfc27bd9e5899376a4f7968ad76dd6c
Joshua Hyde <jrh3k5@gmail.com>
2025-06-07 17:06:04 -0500

    Report, enforce minimum test coverage

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ["coverage/**"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
